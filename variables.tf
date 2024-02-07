64f7195c1fc1e401408a677d4b7b0fb16ff0c624
Pascal Laroche <16697148+plaroche1978@users.noreply.github.com>
2024-02-06 19:35:34 -0500

    region add

--- variables.tf
+++ variables.tf
@@ -10,3 +10,8 @@ variable "db_password" {
   description = "RDS root user password"
   sensitive   = true
 }
+
+variable "region" {
+  description = "Region to deploy RDS instance in. Default is ca-central-1."
+  default   = "ca-central-1"
+}
